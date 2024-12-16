533649b68307d68b1b62f971b4294b1e3419e1e2
Ayymer <82091839+Ayymer@users.noreply.github.com>
2024-12-16 13:39:55 +0100

    update

--- sketch.js
+++ sketch.js
@@ -2,24 +2,38 @@ function setup() {
   createCanvas(400, 400);
 }
 
-var size = 20;
+var size = 30;
 var sinus1 = 0;
 var sinus2 = 0;
 var sinus3 = 0;
 var pos = 50;
 var offsetPhase = 0;
 
+var r = 100;
+var angle = 0;
+
 function draw() {
   background(220);
   fill(0);
-  for (let i = 0; i < 20; i++) {
-    circle(200, pos, size*sin(offsetPhase+frameCount*0.1));
-    pos += 15;
-    offsetPhase += 0.5;
+
+  for (let i = 0; i < 80; i++) {
+
+    var s = size*(sin(offsetPhase+frameCount*0.02));
+    s = constrain(s, 10, 100);
+
+    circle(200+r*cos(angle),200+r*sin(angle),s);
+    // pos += 15;
+    angle += 360/40;
+    // r+= 1.5;
+    offsetPhase += 0.12;
+
   }
 
-  pos = 50;
+  r = 100;
+  angle = 0;
+  // pos = 50;
   offsetPhase = 0;
+
 
 }
 
